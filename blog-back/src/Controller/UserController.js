88391586670d3656f159b6564b57acc2e687077b
Qulamali <z.qulamali@gmail.com>
2024-02-10 23:46:58 +0400

    add

---
+++ blog-back/src/Controller/UserController.js
@@ -0,0 +1,28 @@
+ export const registerUser=(req,res,next)=>{
+    res.jsons("Register User")
+}
+
+
+export const loginUser=(req,res,next)=>{
+    res.jsons("Login User")
+}
+
+export const getUser=(req,res,next)=>{
+    res.jsons("User Profile")
+}
+
+ export const changePhotoUser=(req,res,next)=>{
+    res.jsons("User User Photo")
+}
+
+
+export  const editUser=(req,res,next)=>{
+    res.jsons("Edit User Profile")
+}
+
+const getAuthors=(req,res,next)=>{
+    res.jsons("Get all users")
+}
+
+
+
---
+++ blog-back/src/Routes/UserRoutes.js
@@ -0,0 +1,1 @@
+require("../Controller/UserController.js")
