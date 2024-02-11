b7eb45d5a8fad7a3e98776bd83eed3d7ba955a37
Qulamali <z.qulamali@gmail.com>
2024-02-12 01:34:58 +0400

    add

---
+++ blog-back/src/Model/UserModel.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+
+    },
+    posts: {
+        type: Number,
+        default: 0
+    },
+    role: {
+        type: String,
+        default: 'User'
+    },
+}, { timestamps: true });
+
+export const UsersModel = mongoose.model('Users', userSchema);
